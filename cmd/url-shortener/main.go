4ac63690bfebe8bb4dbd3b4bdb6973e12adf0ffa
Raúl Enrique González <rgonzalez@netelcomunicaciones.es>
2024-10-06 09:45:14 +0200

    Commit inicial
    ROADMAP TODO

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+
+	// ROADMAP of project
+
+	// TODO: init config: cleanenv
+
+	// TODO: init Logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO:init router: chi, chi-render
+
+	// TODO: run server
+}
